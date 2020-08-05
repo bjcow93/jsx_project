5c94a74121393002f3338e2ca11a8edeee5cd058
Ben Cowden <benjcowden@gmail.com>
2020-08-05 08:54:18 -0700

    Finish

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const title = <h1>Click an animal for a fun fact!</h1>;
+let background = '/images/ocean.jpg';
+
+const animals = {
+  dolphin: {
+    image: '/images/dolphin.png',
+    facts: ["Dolphins have been shown to give distinct names to each other!", "Dolphins are known to display their own culture!"]
+  },
+  lobster: {
+    image: '/images/lobster.png',
+    facts: ["Lobsters taste with their legs!", "Lobsters chew with their stomachs!"]
+  },
+  starfish: {
+    image: '/images/starfish.png',
+    facts: ["Starfish can have up to 40 arms!", "Starfish do not have any blood!"]
+  }
+};
+
+
+function displayFact(e) {
+  const clicked = e.target.alt;
+  const option = Math.random() < 0.5 ? 0 : 1;
+  document.getElementById("fact").innerHTML = animals[clicked].facts[option];
+}
+
+
+const animalFacts = (
+  <div>
+
+    {title}
+    <img className="ocean" alt="ocean" src={background ? background : '/images/ocean2.jpg'} />;
+
+
+    <div className="animals">
+      {Object.keys(animals).map((animal, idx) => (
+        <img
+          key={idx}
+          className="animal"
+          alt={animal}
+          src={animals[animal].image}
+          onClick={displayFact}
+        />
+      ))}
+    </div>
+
+    <div id="fact"></div>
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(
+  animalFacts,
+  document.getElementById('root')
+);
