ff7884482f1bdc7d832af52a4d837ecee28cd993
Ben Cowden <benjcowden@gmail.com>
2020-08-06 14:08:28 -0700

    Update

--- src/index.js
+++ src/index.js
@@ -5,23 +5,23 @@ const title = '';
 
 const showBackground = true;
 const background = <img
-  className="background"
-  alt="ocean"
+  className='background'
+  alt='ocean'
   src='/images/ocean.jpg'
 />;
 
 const animals = {
   dolphin: {
     image: '/images/dolphin.png',
-    facts: ["Dolphins have been shown to give distinct names to each other!", "Dolphins are known to display their own culture!"]
+    facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!']
   },
   lobster: {
     image: '/images/lobster.png',
-    facts: ["Lobsters taste with their legs!", "Lobsters chew with their stomachs!"]
+    facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!']
   },
   starfish: {
     image: '/images/starfish.png',
-    facts: ["Starfish can have up to 40 arms!", "Starfish do not have any blood!"]
+    facts: ['Starfish can have up to 40 arms!', 'Starfish do not have any blood!']
   }
 };
 
@@ -30,11 +30,11 @@ for (const animal in animals) {
   images.push(
   <img
     key={animal}
-    className="animal"
+    className='animal'
     alt={animal}
     src={animals[animal].image}
     aria-label={animal}
-    role="button"
+    role='button'
     onClick={displayFact}
   />)
 }
@@ -44,15 +44,15 @@ function displayFact(e) {
   const optionIndex = Math.floor(Math.random() * selectedAnimal.facts.length);
   const funFact = selectedAnimal.facts[optionIndex];
 
-  document.getElementById("fact").innerHTML = funFact;
+  document.getElementById('fact').innerHTML = funFact;
 }
 
 const animalFacts = (
   <div>
     <h1>{title === '' ? 'Click an animal for a fun fact!' : title}</h1>
-    <div id="fact"></div>
     {showBackground && background}
-    <div className="animals">
+    <p id='fact'></p>
+    <div className='animals'>
       {images}
     </div>
   </div>
@@ -63,3 +63,6 @@ ReactDOM.render(
   animalFacts,
   document.getElementById('root')
 );
+
+
+//toggle background extra credit maybe
